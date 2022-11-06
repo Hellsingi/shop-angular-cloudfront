a49e232fa854e1e36bdfbfe7fb3039c16af4dcf3
Hellsingi <il.dzemchanka@gmail.com>
2022-11-06 22:29:09 +0100

    feat: change env for carts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,7 +11,7 @@ export const environment: Config = {
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://kfb1wfjj8c.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://ei5jgu2ft4.execute-api.eu-west-1.amazonaws.com/dev',
-    cart: 'https://6n1wnohoii.execute-api.eu-west-1.amazonaws.com/dev/',
+    cart: 'http://hellsingi-task-api2.eu-west-1.elasticbeanstalk.com/api',
   },
   apiEndpointsEnabled: {
     product: false,
